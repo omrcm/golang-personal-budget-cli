f679850032e6a179eca9dc3b6d9b40b20c8326a7
Ömür Uçum <omurucum@gmail.com>
2022-12-07 05:02:36 +0300

    Module 3 Task 2

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -74,7 +74,7 @@ func (b *Budget) RemoveItem(description string) {
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
-	if len(report) > 12 {
+	if len(report) >= 12 {
 		return nil, errReportIsFull
 	}
 
